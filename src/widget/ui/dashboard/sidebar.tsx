d9185552544a2b3afafed560672a224697fcd7fb
Navi <ihuayhuam@unsa.edu.pe>
2025-08-06 17:44:14 -0500

    Modify Structure for Buttons - Sidebar

--- src/widget/ui/dashboard/sidebar.tsx
+++ src/widget/ui/dashboard/sidebar.tsx
@@ -1,5 +1,5 @@
-import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarProvider } from '@/shared/components/ui/sidebar'
-
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader } from '@/shared/components/ui/sidebar'
+import { Button } from '@/shared/components/ui/button'
 
 export default function DashboardWidgetSidebar() {
 
@@ -8,14 +8,13 @@ export default function DashboardWidgetSidebar() {
       <Sidebar variant="floating" className="relative h-full">
         <SidebarHeader/>
         <SidebarContent>
-          <SidebarGroup >
-            <SidebarGroupLabel>
-              GESTIÓN
-            </SidebarGroupLabel>
+          <SidebarGroup>
             <SidebarGroupContent>
-              <SidebarMenu>
-                {}
-              </SidebarMenu>
+              <div className='flex flex-col gap-4 mt-1'>
+                <Button>Asesores</Button>
+                <Button>Visitas</Button>
+                <Button>Clientes</Button>
+              </div>
             </SidebarGroupContent>
           </SidebarGroup>
           <SidebarGroup />
