0584b76924b4abbcf0027e2d24c84d145fdb9176
Darkfrost-1107 <gabrielbernedo610@gmail.com>
2025-08-06 10:38:54 -0500

    fix: use router ocmpletely isolated

--- src/widget/ui/login/index.tsx
+++ src/widget/ui/login/index.tsx
@@ -2,11 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import LoginWidget from './loginWidget';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <LoginWidget />
-  </React.StrictMode>
-);
 export { LoginWidget };
 
