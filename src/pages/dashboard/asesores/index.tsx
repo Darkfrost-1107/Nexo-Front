37e6a19e8ba35a781d0d9b45aacd738eb9e224ec
Navi <ihuayhuam@unsa.edu.pe>
2025-08-06 12:23:23 -0500

    folders creation for asesores

---
+++ src/pages/dashboard/asesores/index.tsx
@@ -0,0 +1,11 @@
+import PresentationWidget from '@/widget/ui/presentation'
+
+export default function AsesoresDashboardPage() {
+  return (
+    <PresentationWidget 
+      src="https://avatars.githubusercontent.com/u/123456789?v=4"
+      alt="User Avatar"
+      title="Bienvenido pendejo"
+    />
+  )
+}
