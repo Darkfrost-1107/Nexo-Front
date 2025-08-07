5ce77e7c627b9732bb59468bdcd145447b71eeaa
Navi <ihuayhuam@unsa.edu.pe>
2025-08-07 12:25:57 -0500

    update: modify visitasWidget

--- src/widget/ui/visitas/visitasWidget.tsx
+++ src/widget/ui/visitas/visitasWidget.tsx
@@ -1,7 +1,18 @@
-import VisitasTable from "./visitasTable"
-
+import VisitasTable from "./visitasTable";
+import SearchForm from "./visitasSearchForm";
 export default function VisitasDashboardPage() {
   return (
-    <VisitasTable />
-  )
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-6">Visitas</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        <div className="lg:col-span-2">
+          <SearchForm />
+        </div>
+      </div>
+
+      <div className="mt-8">
+        <VisitasTable />
+      </div>
+    </div>
+  );
 }
