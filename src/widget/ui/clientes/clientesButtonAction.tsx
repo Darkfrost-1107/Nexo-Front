aa4619c457debc9a7e136b946cb2c2ad2a19abfb
Navi <ihuayhuam@unsa.edu.pe>
2025-08-07 13:11:55 -0500

    update: button actions

---
+++ src/widget/ui/clientes/clientesButtonAction.tsx
@@ -0,0 +1,9 @@
+import {Button} from '@/shared/components/ui/button'
+export default function ActionButtons() {
+  return (
+    <div className="flex flex-col gap-4 mt-1">
+        <Button size="sm" className="text-white"> Generar Reportes </Button>
+        <Button size="sm" className="text-white"> Crear Cliente </Button>
+    </div>
+  );
+}
--- src/widget/ui/clientes/clientesWidget.tsx
+++ src/widget/ui/clientes/clientesWidget.tsx
@@ -1,3 +1,6 @@
+import SearchForm from "./clientesSearchForm";
+import ActionButtons from "./clientesButtonAction";
+import ClientesTable from "./clientesTable";
 
 export default function ClientesPage() {
   return (
@@ -5,15 +8,15 @@ export default function ClientesPage() {
       <h1 className="text-3xl font-bold mb-6">Clientes</h1>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
         <div className="lg:col-span-2">
-          {/* <SearchForm /> */}
+          <SearchForm />
         </div>
         <div className="flex flex-col justify-start items-end gap-4">
-          {/* <ActionButtons /> */}
+          <ActionButtons />
         </div>
       </div>
 
       <div className="mt-8">
-        {/* <AsesoresTable /> */}
+        <ClientesTable />
       </div>
     </div>
   );
