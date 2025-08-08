0965b7355d556a2d46ebf639ff2d8ae8454f6429
Navi <ihuayhuam@unsa.edu.pe>
2025-08-08 11:00:11 -0500

    update: button

--- src/widget/ui/clientes/clientesButtonAction.tsx
+++ src/widget/ui/clientes/clientesButtonAction.tsx
@@ -1,9 +1,12 @@
 import {Button} from '@/shared/components/ui/button'
+import  CrearClientDialog from './clientesFormCreate';
+
 export default function ActionButtons() {
   return (
     <div className="flex flex-col gap-4 mt-1">
         <Button size="sm" className="text-white"> Generar Reportes </Button>
-        <Button size="sm" className="text-white"> Crear Cliente </Button>
+        {/* <Button size="sm" className="text-white"> Crear Cliente </Button> */}
+        <CrearClientDialog />
     </div>
   );
 }
