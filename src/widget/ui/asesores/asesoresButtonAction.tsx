9998a9a09c7d5e56eb711f5ed721a1b7d635b628
Navi <ihuayhuam@unsa.edu.pe>
2025-08-08 11:40:54 -0500

    update: update button

--- src/widget/ui/asesores/asesoresButtonAction.tsx
+++ src/widget/ui/asesores/asesoresButtonAction.tsx
@@ -1,14 +1,11 @@
 import {Button} from '@/shared/components/ui/button'
-// import {IconGitBranch} from '@tabler/icons-react';
+import CrearAsesorDialog from './asesoresFormCreate';
+
 export default function ActionButtons() {
   return (
-    // <div className="action-buttons">
-    //   <button type="button">📄 Generar Reporte</button>
-    //   <button type="button">👥 Crear asesor</button>
-    // </div>
     <div className="flex flex-col gap-4 mt-1">
         <Button size="sm" className="text-white"> Generar Reportes </Button>
-        <Button size="sm" className="text-white"> Crear Asesor </Button>
+        <CrearAsesorDialog />
     </div>
   );
 }
