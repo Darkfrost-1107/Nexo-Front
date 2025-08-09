fee6d4ea1e7ecf007bc56d92f02be8dac4ceaac8
Darkfrost-1107 <gabrielbernedo610@gmail.com>
2025-08-09 17:57:27 -0500

    feat: arr jwt error interceptor

--- src/shared/api/interceptors/jwt-interceptor.ts
+++ src/shared/api/interceptors/jwt-interceptor.ts
@@ -1,13 +1,29 @@
+import StatusCodes from "@/shared/constants/status-codes"
 import { userStore } from "@/shared/stores/credentials"
+import type { AxiosError } from "axios"
 
-function JWTInterceptor(config : ServiceRequestConfig) {   
+export function JWTInterceptor(config : ServiceRequestConfig) {   
     let jwt = userStore.getState().jwt
     if(jwt){
         config.headers["Authorization"] = `Bearer ${jwt}`
     }
     // config.headers["Access-Control-Allow-Origin'"] = "*"
     return config
-
 }
 
-export default JWTInterceptor
+export function JWTErrorInterceptor(error: AxiosError){
+    
+    if(error.response){
+        let {code} = error.response.data as {code: string} 
+        switch(code){
+            case StatusCodes.TOKEN_NOT_VALID.code:
+                userStore.setState({
+                    jwt: undefined,
+                    user: undefined
+                })
+                window.location.assign("/login")
+        }
+    }
+
+    return Promise.reject(error)
+}
