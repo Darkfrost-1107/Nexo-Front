61825b5319c162fdbb95a9bd594be6f361d491d2
Darkfrost-1107 <gabrielbernedo610@gmail.com>
2025-08-08 23:32:42 -0500

    update: ui display

--- src/features/login/ui.tsx
+++ src/features/login/ui.tsx
@@ -1,6 +1,10 @@
+import UsersService from '@/entities/users/api'
 import useUserUI from '@/entities/users/ui'
+import { Button } from '@/shared/components/ui/button'
 import { Form } from '@/shared/components/ui/form'
-import React from 'react'
+import { userStore } from '@/shared/stores/credentials'
+import React, { useRef } from 'react'
+import { Link, useNavigate } from 'react-router'
 
 export default function LoginFeatureUI() {
     
@@ -8,21 +12,55 @@ export default function LoginFeatureUI() {
         components: {
             UserPassword,
             UserUsername,
-        }
+        },
+        form
     } = useUserUI()
 
+    const pending = useRef(false)
+    const navigate = useNavigate()
+
+    function HandleSubmit({username, password}: UserData){
+        if(pending.current) return;
+
+        pending.current = true
+        
+        const service = new UsersService()
+        async function fetch(){
+            const {access, user} = await service.login(username, password)
+            userStore.setState({
+                jwt: access
+            })
+            console.log(user)
+            navigate("/dashboard")
+            pending.current = false
+        }
+        fetch().catch(() => {
+            pending.current = false
+        })
+    }
   
     return (
-        <Form>
-            <div className="form-group">
-                <label htmlFor="username">Usuario</label>
-                <UserUsername />
-            </div>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(HandleSubmit)}>
+                <div className="form-group">
+                    {/* <label htmlFor="username">Usuario</label> */}
+                    <UserUsername />
+                </div>
 
-            <div className="form-group">
-                <label htmlFor="password">Contraseña</label>
-                <UserPassword />
-            </div>
+                <div className="form-group">
+                    {/* <label htmlFor="password">Contraseña</label> */}
+                    <UserPassword />
+                </div>
+                <Button 
+                    // asChild 
+                    type="submit"
+                    variant="outline" 
+                    className="w-full"
+                >
+                    {/* <Link to="/login">Login</Link> */}
+                    Login
+                </Button>
+            </form>
         </Form>
     )
 }
