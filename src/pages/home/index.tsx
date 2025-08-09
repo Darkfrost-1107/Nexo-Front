b51505defaae8fd625325a15ea877a1a2a2aec7c
Darkfrost-1107 <gabrielbernedo610@gmail.com>
2025-08-09 17:58:36 -0500

    review: add redirect to login from home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,13 @@
 import DashboardWidget from '@/widget/ui/dashboard'
+import { useNavigate } from 'react-router'
 
 export default function HomePage() {
+  const navigate = useNavigate()
+  setTimeout( () => {
+    navigate("/login")
+
+  }, 1000)
+  
   return (
     <DashboardWidget />
   )
