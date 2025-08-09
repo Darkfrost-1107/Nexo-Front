53e79ef6a3c252b8639dbb6b6e76531e73782481
Darkfrost-1107 <gabrielbernedo610@gmail.com>
2025-08-09 17:56:26 -0500

    feat: add status code for actions

---
+++ src/shared/constants/status-codes.ts
@@ -0,0 +1,32 @@
+const StatusCodes = {
+    TOKEN_NOT_VALID: {
+        code: "token_not_valid",
+        message: "aea"
+    },
+    REQUEST_COMPLETED: {
+        code: "request_completed",
+        message: "Request Completed !!!"
+    },
+    NETWORK_ERROR: {
+        code: "network_error",
+        message: "Cannot Connect With Server"
+    },
+    SERVER_INTERNAL_ERROR: {
+        code: "server_internal_error",
+        message: ""
+    },
+    UNKNWON_ERROR : {
+        code: "unknown_error",
+        message: ""
+    },
+    INVALID_CREDENTIALS : {
+        code: "invalid_credentials",
+        message: "Check your Username or Password"
+    },
+    UNAUTHORIZED: {
+        code: "unauthorized",
+        message: "You are not authorized !!!"
+    }
+}
+
+export default StatusCodes
