8c680cd361636c7da918f6f767aeb231bb40830d
Darkfrost-1107 <gabrielbernedo610@gmail.com>
2025-08-08 23:31:38 -0500

    review: changing validator to accept username and password only

--- src/entities/users/model/validators.ts
+++ src/entities/users/model/validators.ts
@@ -2,15 +2,16 @@ import ObjectStatus from "@/shared/constants/status";
 import z from "zod";
 
 const validators : z.ZodType<UserData> = z.object({
-    id: z.uuid(),
+    // id: z.uuid(),
     username: z.string(),
-    names: z.string(),
-    email: z.email(),
-    phone_number: z.string(),
-    user_type: z.string(), // Adjust type if it's an enum or specific values
-    created_date: z.string(), // or z.date() if it's a Date
-    updated_date: z.string(), // or z.date()
-    status: z.nativeEnum(ObjectStatus), // Use z.nativeEnum or z.enum([...]) to match ObjectStatus
+    // names: z.string(),
+    // email: z.email(),
+    // phone_number: z.string(),
+    // user_type: z.string(), // Adjust type if it's an enum or specific values
+    // created_date: z.string(), // or z.date() if it's a Date
+    // updated_date: z.string(), // or z.date()
+    // status: z.nativeEnum(ObjectStatus), // Use z.nativeEnum or z.enum([...]) to match ObjectStatus
+    password: z.string()
     // Add other required fields from UserData here, for example:
     // last_name: z.string(),
     // created_at: z.string(), // or z.date() if it's a Date
