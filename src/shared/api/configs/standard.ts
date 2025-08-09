7a823a7a7adf9e83e95a64f2a68f81e718d553d2
Darkfrost-1107 <gabrielbernedo610@gmail.com>
2025-08-09 17:56:51 -0500

    fix | review : change response in services standard

--- src/shared/api/configs/standard.ts
+++ src/shared/api/configs/standard.ts
@@ -1,3 +1,6 @@
+import StatusCodes from "@/shared/constants/status-codes";
+import { AxiosError } from "axios";
+
 class StandardApiService {
     protected config: ServiceConfig;
     protected handlers?: FlowHandlers;
@@ -12,13 +15,37 @@ class StandardApiService {
             const response = await this.config.connector.request(config);
             this.handlers?.onSuccess?.(response);
             if(this.config.debug){
-                console.log(`${action || 'Request'} successful:`, response);
+                console.log(`${action || 'Request'} successful: `, response);
             }
-            return response.data;
+            return {
+                status: StatusCodes.REQUEST_COMPLETED,
+                data: response.data
+            };
         } catch (error) {
-            console.error('Error listing items:', error);
+            
+            if(this.config.debug){
+                console.error(`Error in ${action || 'Request'}: `, error);
+            }
             this.handlers?.onError?.(error);
-            throw error;
+
+            if(!(error instanceof AxiosError)){
+                return {
+                    status: StatusCodes.UNKNWON_ERROR,
+                    data: error
+                }
+            }
+
+            if(error.status == 401){
+                return {
+                    status: StatusCodes.UNAUTHORIZED,
+                    data: error
+                }
+            }
+
+            return {
+                status: StatusCodes.SERVER_INTERNAL_ERROR,
+                data: error,
+            }
         }
     }
 }
